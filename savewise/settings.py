0115baf7e3cc19107ad8cadc0ccb82a357df4c2a
Houzsaad <227036578+Houzsaad@users.noreply.github.com>
2026-08-14 17:24:43 -0700

    build wallet endpoints that query balance and date only

--- savewise/settings.py
+++ savewise/settings.py
@@ -47,8 +47,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'accounts',
-    
-    'wallet'
+    'wallet',
 
 ]
 
--- wallet/views.py
+++ wallet/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .serializers import WalletSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, status
+
+class WalletView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = WalletSerializer
+
+    def get_object(self):
+        return self.request.user.wallet
 
 # Create your views here.
